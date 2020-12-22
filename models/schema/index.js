c2044955706ba721b0ba20ccb8b38319a06b3df3
Kim Taeyeon <teerapatpor@gmai.com>
2020-12-22 15:20:15 +0700

    split phone check and send otp

--- models/schema/index.js
+++ models/schema/index.js
@@ -1,7 +1,6 @@
 const userSchema = require("./user");
 const userInfoSchema = require("./userInfo");
 const technicianSchema = require("./technicianInfo");
-const otpSchema = require("./otp");
 const formSchema = require("./form");
 const imageSchema = require("./image");
 
@@ -9,7 +8,6 @@ module.exports = {
   userSchema,
   userInfoSchema,
   technicianSchema,
-  otpSchema,
   formSchema,
   imageSchema,
 };
--- models/schema/otp.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const otpSchema = new mongoose.Schema({
-  code: String,
-});
-
-module.exports = otpSchema;
