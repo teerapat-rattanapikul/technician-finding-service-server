811fd08b88087f5867bba63e76e359a8bdfc0ffd
Kim Taeyeon <teerapatpor@gmai.com>
2020-12-22 15:04:12 +0700

    send oTP

--- apiConnect.js
+++ apiConnect.js
@@ -1,30 +1,19 @@
 register;
 
 //-------------------------------------------
-1`เช็คการซ้ำกันของ username`// url : http://localhost:9999/api/user
+1`เช็คการซ้ำกันของ username` // url : http://localhost:9999/api/user
 `query{
         usernameCheck(username:"${username}")
-    }``   return true ถ้า username สามารถใช้ได้
-        return false ถ้า username ซ้้ำ   
-    `;
+    }`;
+
 //-------------------------------------------
-2`เช็คการซ้ำกันของ เบอร์โทรศัพท์ พร้อมส่ง otp`// url : http://localhost:9999/api/otp
-`query{
+2`เช็คการซ้ำกันของ เบอร์โทรศัพท์ พร้อมส่ง otp` // url : http://localhost:9999/api/otp
+`mutation{
         sendOTP(phone:"${phone}")
-    }``   
-        return true ถ้า เบอร์โทร สามารถใช้ได้
-        return false ถ้า เบอร์โทร ซ้้ำ   
-    `;
+    }`;
+
 //-------------------------------------------
-3`ยืนยัน otp `// url : http://localhost:9999/api/otp
-`query{
-        getOTP(code:"${code}")
-    }``   
-        return true ถ้า code ถูกต้อง
-        return false ถ้า code ไม่ถูกต้อง   
-    `;
-//-------------------------------------------
-4`บันทึกข้อมูลการสมัคร`// url : http://localhost:9999/api/user
+4`บันทึกข้อมูลการสมัคร` // url : http://localhost:9999/api/user
 ` mutation{
         register(REGISTER:{
                 username:"${username}",
@@ -42,3 +31,12 @@ register;
             status
         }
     }`;
+
+//-------------------------------------------
+login // url : http://localhost:9999/api/user
+`mutation{
+    login(LOGIN:{username:"${username}",password:"${password}"}){
+        token
+        status
+    }
+}`;
