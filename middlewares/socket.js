78da865f3d780ea951b1117469ad4452665911db
wongwhale <wongwhale_@hotmail.com>
2020-12-25 16:11:11 +0700

    test room emit

--- middlewares/socket.js
+++ middlewares/socket.js
@@ -1,26 +1,47 @@
-var client = []
+var clients = []
 
 module.exports = (app, io, db) => {
     io.on('connection', function (socket) {
         console.log(`${socket.id} connected`);
-        socket.on('join', ({uid}) => {
+        socket.on('join', ({ uid }) => {
             var clientInfo = new Object()
             clientInfo.uid = uid
             clientInfo.sid = socket.id
-            client[uid] = clientInfo
-            // socket.join(room)
-            // socket.join('api')
-            console.log(uid , 'join api room');
+            clients[uid] = clientInfo
+            console.log(clients);
+            socket.join('api')
+            // console.log(uid, 'join api room');
+            console.log(socket.rooms);
             socket.emit('join', { id: socket.id })
         })
-        socket.on('leave', ({uid}) => {
-            // socket.leave('api')
-            console.log(uid , 'leave api room');
+        socket.on('leave', ({ uid }) => {
+            console.log(uid, 'leave api room');
+            socket.leave('api')
+            delete clients[uid]
 
         })
         socket.on('disconnect', function () {
-            console.log(client);
-            console.log(`${socket.id} disconnected`);
+            console.log(clients, socket.id);
+            for (var client in clients) {
+                console.log(client);
+                if (clients[client].sid === socket.id) {
+                    delete clients[client]
+                    break
+                }
+            }
         })
+
+        socket.on('send_post_req', function (data) {
+            console.log(data)
+            socket.broadcast.to('api').emit('send_post_req' , {data})
+        })
+
+        socket.on('send_message' , function(data) {
+            // console.log(data);
+            socket.broadcast.to('api').emit('receive_message' , {data})
+        })
+
+        
     })
+
 }
