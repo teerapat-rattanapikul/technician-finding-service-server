6d68eb04ccfcfd856f01a56824a93c6a8bbb1672
Kim Taeyeon <teerapatpor@gmai.com>
2021-01-04 16:15:50 +0700

    add form

--- controllers/form.js
+++ controllers/form.js
@@ -3,15 +3,28 @@ const imageModel = require("../models").images;
 const fs = require("fs");
 
 module.exports = {
-  addForm: async ({ INFORMATION }) => {
+  addForm: async ({ INFORMATION }, req) => {
     try {
       INFORMATION = JSON.parse(JSON.stringify(INFORMATION));
-      console.log(INFORMATION);
+      INFORMATION["date"] = new Date(INFORMATION.date)
+        .toISOString()
+        .replace(/T/, " ")
+        .replace(/\..+/, "");
+      INFORMATION["senderID"] = req.userID;
       const information = await formModel.create(INFORMATION);
       console.log(information);
       return information;
     } catch (error) {
       throw error;
+    }
+  },
+  getForm: async (args) => {
+    try {
+      const form = await formModel.findOne({ _id: args._id });
+      return form;
+    } catch (error) {
+      console.log(error);
+      return error;
     }
   },
   deleteForm: async ({ INFORMATION }) => {
