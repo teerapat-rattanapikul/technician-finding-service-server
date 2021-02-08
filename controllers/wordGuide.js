d2b986f03b470781ffe0dd83c5cb7bb97dce9510
Kim Taeyeon <teerapatpor@gmai.com>
2021-02-08 17:18:36 +0700

    add wordGuide when create Teachnicina

--- controllers/wordGuide.js
+++ controllers/wordGuide.js
@@ -3,10 +3,6 @@ module.exports = {
   wordGuide: async (args, req) => {
     try {
       if (req.userID !== null && req.userID !== undefined) {
-        // const wordGuide = await wordGuideModel.findOne();
-        // const result = wordGuide.word.filter((words) =>
-        //   words.includes(args.word)
-        // );
         const findWord = await wordGuideModel.find({
           word: { $regex: args.word, $options: "i" },
         });
