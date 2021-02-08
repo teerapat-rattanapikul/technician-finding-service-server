568f312b568e18bd6cc3432eeecda75b8e04f99e
Kim Taeyeon <teerapatpor@gmai.com>
2021-02-08 18:49:03 +0700

    remove token from wordGuide

--- controllers/wordGuide.js
+++ controllers/wordGuide.js
@@ -2,15 +2,11 @@ const wordGuideModel = require("../models").words;
 module.exports = {
   wordGuide: async (args, req) => {
     try {
-      if (req.userID !== null && req.userID !== undefined) {
-        const findWord = await wordGuideModel.find({
-          word: { $regex: args.word, $options: "i" },
-        });
-        const result = findWord.map((data) => data.word);
-        return result;
-      } else {
-        return [];
-      }
+      const findWord = await wordGuideModel.find({
+        word: { $regex: args.word, $options: "i" },
+      });
+      const result = findWord.map((data) => data.word);
+      return result;
     } catch (error) {
       throw error;
     }
